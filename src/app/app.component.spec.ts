dbc775ef519d51abb40fd37263a8a29a15318109
Ros Haitovich <matrostik@gmail.com>
2018-01-13 00:12:49 +0200

    steps 1-7

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, HeroDetailComponent
       ],
     }).compileComponents();
   }));
